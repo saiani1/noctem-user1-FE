408badd9bf8036b8c748816f3104264f29f23798
Park su ah <tndk4997@naver.com>
2022-09-26 20:35:47 +0900

    Temp: main 임시저장

---
+++ pages/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import HomeContent from '../src/components/homeContent';
+// import Header from '../components/common/Header';
+// import ToolbarList from '../src/types/toolbar';
+
+function mainPage() {
+  return (
+    <>
+      {/* <Header /> */}
+      <HomeContent />
+      {/* <ToolbarList /> */}
+      main
+    </>
+  );
+}
+
+export default mainPage;
