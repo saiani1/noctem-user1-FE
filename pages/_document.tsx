e273dca56e8037b753fd7fbe4a1981ebbae3de01
Im-hass <huiseon4107@gmail.com>
2022-09-27 17:38:32 +0900

    Fix: viewport 속성 이동, header console 에러 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,10 +22,6 @@ class MyDocument extends Document {
         <Head>
           <meta charSet='utf-8' />
           <meta name='theme-color' content='#FFFFFF' />
-          <meta
-            name='viewport'
-            content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
-          />
           <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
           <link rel='manifest' href='/manifest.json' />
         </Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,10 @@ const Home: NextPage = () => {
       <Head>
         <title>Nectem Order</title>
         <meta name='description' content='Nectom Order' />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+        />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div>
