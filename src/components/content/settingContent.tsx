90aecf1ff28449a78c6e781c3fc0d695229666bd
MJ CHOI <saiani@naver.com>
2022-10-04 11:52:09 +0900

    Feat: 설정 API연결 / 기능구현 완료

--- src/components/content/settingContent.tsx
+++ src/components/content/settingContent.tsx
@@ -6,16 +6,16 @@ import classNames from 'classnames/bind';
 import styles from '../../../styles/content/settingContent.module.scss';
 
 interface IInfo {
-  value: string;
-  isDarkmode: boolean;
-  pushNotificationAgreement: boolean;
-  advertisementAgreement: boolean;
-  useLocationInfoAgreement: boolean;
-  shakeToPay: boolean;
+  isDarkmode?: boolean;
+  pushNotificationAgreement?: boolean;
+  advertisementAgreement?: boolean;
+  useLocationInfoAgreement?: boolean;
+  shakeToPay?: boolean;
 }
 
 function settingContent() {
   const [info, setInfo] = useState<IInfo>();
+  const [isFetching, setIsFetching] = useState(false);
   const cx = classNames.bind(styles);
 
   useEffect(() => {
@@ -29,7 +29,8 @@ function settingContent() {
           },
         })
         .then((res: any) => {
-          console.log(res.data.data);
+          setInfo(res.data.data);
+          setIsFetching(true);
         })
         .catch(err => {
           console.log(err);
@@ -37,81 +38,99 @@ function settingContent() {
     }
   }, []);
 
-  // const handleChangePush = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   if (info?.pushNotificationAgreement !== e.target.checked) {
-  //     axios.patch()
-  //   }
-  // };
-  // const handleChangeEvent = (e: React.ChangeEvent<HTMLInputElement>) => {};
-  // const handleChangeLocation = (e: React.ChangeEvent<HTMLInputElement>) => {};
-  // const handleChangeShake = (e: React.ChangeEvent<HTMLInputElement>) => {};
-  // const handleChangeDark = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const handleChangeOption = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const check = e.target.checked;
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      axios
+        .patch(
+          `http://121.145.206.143:8000/api/user-service/optionalInfo/${value}`,
+          null,
+          {
+            headers: {
+              Authorization: JSON.parse(token),
+            },
+          },
+        )
+        .then((res: any) => {
+          console.log(res);
+          setInfo({ ...info, [value]: check });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
 
   return (
     <div className={cx('wrap')}>
       <h2>설정</h2>
-      <ul className={cx('setting-li-wrap')}>
-        <li className={cx('setting-li')}>
-          <label htmlFor='push'>푸쉬 알림</label>
-          <div className={cx('toggle-wrap')}>
-            <input
-              type='checkbox'
-              checked={info?.pushNotificationAgreement}
-              // onChange={handleChangePush}
-              value='pushNotificationAgreement'
-            />
-            <div className={cx('toggle')} />
-          </div>
-        </li>
-        <li className={cx('setting-li')}>
-          <label htmlFor='push'>프로모션/이벤트 알림 수신</label>
-          <div className={cx('toggle-wrap')}>
-            <input
-              type='checkbox'
-              checked={info?.advertisementAgreement}
-              // onChange={handleChangeEvent}
-              value='advertisementAgreement'
-            />
-            <div className={cx('toggle')} />
-          </div>
-        </li>
-        <li className={cx('setting-li')}>
-          <label htmlFor='push'>위치 정보 서비스 이용약관 동의</label>
-          <div className={cx('toggle-wrap')}>
-            <input
-              type='checkbox'
-              checked={info?.useLocationInfoAgreement}
-              // onChange={handleChangeLocation}
-              value='useLocationInfoAgreement'
-            />
-            <div className={cx('toggle')} />
-          </div>
-        </li>
-        <li className={cx('setting-li')}>
-          <label htmlFor='push'>Shake to pay 설정</label>
-          <div className={cx('toggle-wrap')}>
-            <input
-              type='checkbox'
-              checked={info?.shakeToPay}
-              // onChange={handleChangeShake}
-              value='shakeToPay'
-            />
-            <div className={cx('toggle')} />
-          </div>
-        </li>
-        <li className={cx('setting-li')}>
-          <label htmlFor='push'>다크모드</label>
-          <div className={cx('toggle-wrap')}>
-            <input
-              type='checkbox'
-              checked={info?.isDarkmode}
-              // onChange={handleChangeDark}
-              value='isDarkmode'
-            />
-            <div className={cx('toggle')} />
-          </div>
-        </li>
-      </ul>
+      {isFetching && (
+        <ul className={cx('setting-li-wrap')}>
+          <li className={cx('setting-li')}>
+            <label htmlFor='push'>푸쉬 알림</label>
+            <div className={cx('toggle-wrap')}>
+              <input
+                type='checkbox'
+                defaultChecked={info?.pushNotificationAgreement}
+                onChange={handleChangeOption}
+                value='pushNotification'
+              />
+              <div className={cx('toggle')} />
+            </div>
+          </li>
+          <li className={cx('setting-li')}>
+            <label htmlFor='push'>프로모션/이벤트 알림 수신</label>
+            <div className={cx('toggle-wrap')}>
+              <input
+                type='checkbox'
+                defaultChecked={info?.advertisementAgreement}
+                onChange={handleChangeOption}
+                value='advertisement'
+              />
+              <div className={cx('toggle')} />
+            </div>
+          </li>
+          <li className={cx('setting-li')}>
+            <label htmlFor='push'>위치 정보 서비스 이용약관 동의</label>
+            <div className={cx('toggle-wrap')}>
+              <input
+                type='checkbox'
+                defaultChecked={info?.useLocationInfoAgreement}
+                onChange={handleChangeOption}
+                value='location'
+              />
+              <div className={cx('toggle')} />
+            </div>
+          </li>
+          <li className={cx('setting-li')}>
+            <label htmlFor='push'>Shake to pay 설정</label>
+            <div className={cx('toggle-wrap')}>
+              <input
+                type='checkbox'
+                defaultChecked={info?.shakeToPay}
+                onChange={handleChangeOption}
+                value='shakeToPay'
+              />
+              <div className={cx('toggle')} />
+            </div>
+          </li>
+          <li className={cx('setting-li')}>
+            <label htmlFor='push'>다크모드</label>
+            <div className={cx('toggle-wrap')}>
+              <input
+                type='checkbox'
+                defaultChecked={info?.isDarkmode}
+                onChange={handleChangeOption}
+                value='darkmode'
+              />
+              <div className={cx('toggle')} />
+            </div>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
