e063a3f0bdadb30cd5581411be03dc33e1229335
Im-Hass <huiseon4107@gmail.com>
2022-10-10 03:49:25 +0900

    Update: NextAuth Github login 테스트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import type { AppProps } from 'next/app';
+import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
 import '../styles/index.scss';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) => {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <SessionProvider session={pageProps.session}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
   );
 };
 
