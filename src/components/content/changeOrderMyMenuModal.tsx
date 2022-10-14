7a08e38ea84d3462c1697c0f42c6aef5063d1930
Im-hass <huiseon4107@gmail.com>
2022-10-14 13:48:15 +0900

    Fix: import 파일 수정 IMenu -> IMenu1

--- src/components/content/changeOrderMyMenuModal.tsx
+++ src/components/content/changeOrderMyMenuModal.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import classNames from 'classnames/bind';
 
 import styles from '../../../styles/content/changeOrderMyMenu.module.scss';
-import { IMenu } from '../../../src/types/myMenu.d';
+import { IMenu1 } from '../../../src/types/myMenu.d';
 import DndMyMenuItem from '../ui/dndMyMenuItem';
 
 interface IProps {
-  info: IMenu[];
+  info: IMenu1[];
   setIsClickChangeOrderBtn: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
