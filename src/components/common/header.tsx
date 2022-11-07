b28b7617c84e715cfbf11c921f5d0b36e2da11b4
Park su ah <tndk4997@naver.com>
2022-11-07 15:00:22 +0900

    Temp: shake 임시저장

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -44,9 +44,10 @@ function header({ isClose, isBack }: { isClose: boolean; isBack: boolean }) {
       timeout: 1000,
     });
     myShakeEvent.start();
-    window.addEventListener('shake', shakeEventDidOccur, false);
-    // if (shakeState === true) {
-    // }
+    window.addEventListener(`${shakeState}`, shakeEventDidOccur, false);
+    if (shakeState === true) {
+      window.addEventListener('shake', shakeEventDidOccur, false);
+    }
   }, []);
 
   return (
